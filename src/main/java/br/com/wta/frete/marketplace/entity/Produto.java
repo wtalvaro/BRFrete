bd8377eb9073775dbe890bb34a2ca0df6c772d1c
wtalvaro <wagneralvaro80@gmail.com>
2025-11-06 20:01:00 +0000

    refactor: Otimiza imports em Produto.java e ajusta logs de teste
    Ajusta a configuração de logging no application-test.properties para reduzir a verbosidade durante a execução dos testes, silenciando logs de INFO/DEBUG de dependências (root e Flyway).
    Também substitui o import wildcard de JPA por imports explícitos em Produto.java.

--- src/main/java/br/com/wta/frete/marketplace/entity/Produto.java
+++ src/main/java/br/com/wta/frete/marketplace/entity/Produto.java
@@ -1,14 +1,22 @@
 package br.com.wta.frete.marketplace.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 
 import br.com.wta.frete.colaboradores.entity.Lojista;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Mapeia a tabela 'marketplace.produtos'. Representa um produto disponível para
